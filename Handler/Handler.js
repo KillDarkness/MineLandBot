f325553214f029860d2f420c59a1c689d352eadd
KillDarkness <killdarkness45@gmail.com>
2025-03-12 20:10:17 -0300

    🧸 Events Hander Update

--- Handler/Handler.js
+++ Handler/Handler.js
@@ -1,21 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const CommandsHandler = require('./CommandsHandler.js');
+const EventsHandler = require('./EventsHandler.js')
 
 module.exports = (client) => {
     // Carrega o handler de comandos
     CommandsHandler(client);
+    EventsHandler(client);
 
-    // Carrega os eventos
-    const eventsPath = path.join(__dirname, '../Events');
-    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-    for (const file of eventFiles) {
-        const event = require(path.join(eventsPath, file));
-        if (event.once) {
-            client.once(event.name, (...args) => event.execute(...args, client));
-        } else {
-            client.on(event.name, (...args) => event.execute(...args, client));
-        }
-    }
 };
