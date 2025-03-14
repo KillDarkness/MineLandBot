e619a1610759f20b05c2c48732333f14ad3f2e58
KillDarkness <killdarkness45@gmail.com>
2025-03-14 08:58:35 -0300

    💎 Adicção de Handler
    Adicionado Alias e Package

--- Handler/Handler.js
+++ Handler/Handler.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const CommandsHandler = require('./CommandsHandler.js');
-const EventsHandler = require('./EventsHandler.js')
+const EventsHandler = require('./EventsHandler.js');
+const PackageHandler = require('./PackageHandler.js')
 
 module.exports = (client) => {
     // Carrega o handler de comandos
     CommandsHandler(client);
     EventsHandler(client);
+    PackageHandler(client)
+    require('./AliasHandler');
 
 };
