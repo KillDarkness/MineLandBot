e82bb169c9b14ea08fe831c57bcfac4703ed5967
KillDarkness <killdarkness45@gmail.com>
2025-03-18 23:06:41 -0300

    ⚽ Simple Update

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const Handler = require('./Handler/Handler.js');
+const musicBot = require('./musicBot');
 
 const client = new Client({
     intents: Object.values(GatewayIntentBits), // Todas as intents
     partials: Object.values(Partials), // Todos os partials
 });
+
+// Configuração do Riffy e eventos relacionados à música
+musicBot(client);
 
 // Carrega todos os handlers
 Handler(client);
